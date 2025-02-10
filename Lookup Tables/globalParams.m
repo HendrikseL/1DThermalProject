826ca70c936df9d77487b82d24890f1a05bec118
HendrikseL <lukehendrikse@carleton.ca>
2025-02-09 19:41:08 -0500

    updated globalParams

--- Lookup Tables/globalParams.m
+++ Lookup Tables/globalParams.m
@@ -7,10 +7,22 @@ globalInputs.program.radialNodes = 3;
 globalInputs.program.N = 3;
 globalInputs.program.M = 1;
 
+%Aluminum thermophysical properties @300K
+globalInputs.aluminum.rho = 2701; %kg/m^3
+globalInputs.aluminum.cp = 0.902; %kJ/kg K
+globalInputs.aluminum.k = 237; %W/m K
+
+%water thermophysical properties @300K
+globalInputs.water.rho = 996.57; %kg/m^3
+globalInputs.water.cp = 4.18;% kJ/kgK
+globalInputs.water.k = 0.61450; %W/m K
+
 %slurry inputs
 globalInputs.slurry = struct;
 globalInputs.slurry.rho = 1; %slurry density (kg/m^3)
 globalInputs.slurry.Ts_in = 1; %slurry input tepperature (K)
+globalInputs.slurry.massRatio = 1.3; % water/al mass ratio
+% globalInputs.slurry.volumeFraction = ;
 
 
 %screw physical parameters
@@ -23,3 +35,6 @@ globalInputs.screw.omega = 1; %rotational speed of the screw, m
 globalInputs.T_init = 80* ones(8,7);
 globalInputs.T_init(1,3) = 20;
 globalInputs.T_init(1,5) = 20;
+
+globalInputs.HT.h_fpipe = 1900; %W/m^2K
+globalInputs.HT.h_fgasket = 32000; %W/m^2K
