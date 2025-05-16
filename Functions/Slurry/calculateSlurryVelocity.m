80d246382dd138d722e3ee85c4adf95dcbc4f257
HendrikseL <lukehendrikse@carleton.ca>
2025-05-16 13:09:14 -0400

    fixed a bug with mass flow calculation. The mass flow should now produce accurate velocities in the cell.

--- Functions/Slurry/calculateSlurryVelocity.m
+++ Functions/Slurry/calculateSlurryVelocity.m
@@ -9,16 +9,27 @@ radialPosition = globalInputs.program.radialNodes+5  - pos(1);
 ms_ax = globalInputs.slurry.ms * 0.5;
 ms_r = globalInputs.slurry.ms * 0.5;
 
+%turn total mass flow into the portion for that cell
+%axial
+    radius = globalInputs.screw.d/2 + radialPosition*globalInputs.screw.deltaR;
+    A_ax = pi*(radius^2-(radius-globalInputs.screw.deltaR)^2);
+    A_axTotal =(pi/4)*(globalInputs.innerPipe.ID^2-globalInputs.screw.d^2);
+
+    ms_ax = ms_ax* (A_ax/A_axTotal);
+
+%radial
+     x = globalInputs.screw.l /(globalInputs.program.N*globalInputs.program.M);
+    A_r = x * 2*pi*radius;
+    A_rTotal = x*pi*globalInputs.innerPipe.ID;
+
+    ms_r = ms_r * (A_r/A_rTotal);
+
 %no combustion
 if alpha == 0
     %axial velocity
-    radius = globalInputs.screw.d/2 + radialPosition*globalInputs.screw.deltaR;
-    A_ax = pi*(radius^2-(radius-globalInputs.screw.deltaR)^2);
     vel(1) = ms_ax/ (A_ax * rho);
 
     %radial velocity
-     x = globalInputs.screw.l /(globalInputs.program.N*globalInputs.program.M);
-    A_r = x * 2*pi*radius;
     vel(2) = ms_r /(A_r*rho);
 else
     %do nothing
