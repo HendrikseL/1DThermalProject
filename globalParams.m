1aae3a4839854ec8b76f786b9b21909a7656f633
HendrikseL <lukehendrikse@carleton.ca>
2025-06-18 10:30:27 -0400

    fixed pressure gradient calculation. made explicit solver more efficient, program runs much faster now.

--- globalParams.m
+++ globalParams.m
@@ -4,7 +4,7 @@ globalInputs = struct;
 
 %program parameters
 globalInputs.program.radialNodes = 3;
-globalInputs.program.N = 5;
+globalInputs.program.N = 15;
 globalInputs.program.M = 1;
 globalInputs.program.inputPadding = 2;
 globalInputs.program.outputPadding = 2;
--- matrix/explicit solver/explicitSolver.m
+++ matrix/explicit solver/explicitSolver.m
@@ -2,13 +2,16 @@ function T_new = explicitSolver(A,Q,b,Tvec,ElVec,globalInputs,positionMap)
     %explicit solver for the temperature using the coefficient matrix
     T_new = zeros(length(Tvec),1);
     
+    k = 1;
     %inlet boundary condition
     for i = 1:1:2+globalInputs.program.radialNodes
         T_new(i) = Tvec(i);
+        k = k+1;
     end
 
     %solve nodes
-    for i = 2+globalInputs.program.radialNodes+1:1:length(Tvec)-(2+globalInputs.program.radialNodes)
+    i = k;
+    while ~isempty(ElVec{i})
 
         %update velocity and pressure
         if strcmp(ElVec{i}.type,"slurry")
@@ -17,12 +20,15 @@ function T_new = explicitSolver(A,Q,b,Tvec,ElVec,globalInputs,positionMap)
             %find neighbours for ElVec
             [du, d2u] = calculateSlurryVelocityGradient(globalInputs,positionMap,ElVec,i);
 
+            %make sure that for velocity calculations only the old velocity
+            %is used not a new one!!!
+            
             %guess slurry velocity at n+1 for velocity using n pressure
             %field
             ElVec = calculateSlurryVelocity(globalInputs,ElVec,i,du,d2u);
 
             %calculate pressure gradient at n+1
-            % ElVec = calculateSlurryPressureGradient(globalInputs,ElVec,i,du,d2u);
+            ElVec = calculateSlurryPressureGradient(globalInputs,ElVec,i,du,d2u);
 
             %correct the velocity field to respect continuity
             ElVec = calculateSlurryVelocity(globalInputs,ElVec,i,du,d2u);
@@ -30,11 +36,15 @@ function T_new = explicitSolver(A,Q,b,Tvec,ElVec,globalInputs,positionMap)
 
         dT = (globalInputs.program.timeStep/(ElVec{i}.rho*ElVec{i}.c*ElVec{i}.vol))*-(A(i,:)*Tvec' + b(i) - Q(i));
         T_new(i) = Tvec(i) + dT;
+
+        i = i +1;
     end
 
+    k = i;
     %outlet boundary
-    for i = length(Tvec):-1:length(Tvec)-(1+globalInputs.program.radialNodes)
-        T_new(i) = -(A(i,:)*T_new+ b(i) - Q(i));
+    while k < length(ElVec) + 1
+        T_new(k) = -(A(k,:)*T_new+ b(k) - Q(k));
+        k = k +1;
     end
 
 end
